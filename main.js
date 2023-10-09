5d9816dfa234771dc9720ca515af4922e094df6e
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-10-09 19:58:22 +0900

    [Finish]データ構造編#02

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 
 {
   const scores = [
-    70, 
-    90, 
-    80, 
+    70,
+    90,
+    80,
     85
   ];
-
+  console.log(scores[2]);
+  console.log(scores.length);
+  scores[1] = 95;
   console.log(scores);
 }
