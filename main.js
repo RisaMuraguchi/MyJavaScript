5e049e81ef143afe055a0ce69eacb61494383e02
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-10-12 21:06:12 +0900

    [Finish]DOM操作編#02

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
 'use strict';
 
 {
-  const scores = {
-    'math': 80,
-    'english': 90
-  };
-  let sum = 0;
-
-  const entries = Object.entries(scores); //オブジェクトを配列にする
-
-  entries.forEach((prop) => {
-    sum += prop[1]
-    console.log(`${prop[0]}: ${prop[1]}`);
-    console.log(`Sum: ${sum}`);
-    console.log(`Average: ${sum / entries.length}`);
+  document.querySelector('button').addEventListener('click', () => {
+    console.log('Clicked');
   });
-
-
-
 }
