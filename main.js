f7023190df1fb46b153df17213f0ca5baf443423
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-10-14 07:05:10 +0900

    [Finish]DOM操作編#06

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    // if (document.querySelector('p').classList.contains('pink-bg') === false) {
-    //   document.querySelector('p').classList.add('pink-bg');
-    // } else {
-    //   document.querySelector('p').classList.remove('pink-bg');
-    // }
-    document.querySelector('p').classList.toggle('pink-bg');
+    // document.querySelectorAll('li')[0].textContent = 'Changed!';
+    // document.querySelectorAll('li')[1].textContent = 'Changed!';
+    // document.querySelectorAll('li')[2].textContent = 'Changed!';
+    document.querySelectorAll('li').forEach((li) => {
+      li.textContent = 'Changed!';
+    });
   });
 }
