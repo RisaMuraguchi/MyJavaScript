2d8646ef79387722d59a8221af93e665a7886eac
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-10-14 07:18:32 +0900

    [Finish]DOM操作編#07

--- main.js
+++ main.js
@@ -2,11 +2,20 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    // document.querySelectorAll('li')[0].textContent = 'Changed!';
+    document.querySelectorAll('#second')[0].textContent = 'Changed!';
     // document.querySelectorAll('li')[1].textContent = 'Changed!';
     // document.querySelectorAll('li')[2].textContent = 'Changed!';
-    document.querySelectorAll('li').forEach((li) => {
-      li.textContent = 'Changed!';
-    });
+    // document.querySelectorAll('.target').forEach((li) => {
+    //   li.textContent = 'Changed!';
+    // });
   });
 }
+
+// forEach使える、CSSセレクタ全て利用可
+querySelector('li:nth-child(2)')
+querySelectorAll('ul .target')
+
+// forEach不可
+getElementById('second')
+getElementsByTagName('p')
+getElementsByClassName('target')
