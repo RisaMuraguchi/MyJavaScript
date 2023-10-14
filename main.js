17df91380a5217d41db290bf9e8ef3f0c287a397
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-10-14 21:21:25 +0900

    [Finish]DOM操作編#15

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 'use strict';
 
 {
-  document.querySelector('button').addEventListener('click', () => {
-    const colors = [];
-    document.querySelectorAll('input').forEach((checkbox) => {
-      if (checkbox.checked === true) {
-        colors.push(checkbox.value);
-      }
-    });
-    alert(colors.join(','));
+  document.querySelector('input').addEventListener('input', () => {
+    const pElement = document.querySelector('p');
+    const inputElement = document.querySelector('input');
+    pElement.textContent = inputElement.value.length;
   });
 }
 
