97fb806a873f877f4aa1f0462bc8234480d8bdff
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-10-15 08:11:20 +0900

    [Finish]数値操作編#03

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 {
-  document.querySelector('button').addEventListener('click', () => {
-    document.querySelector('p').style.fontSize = '24px';
-  });
+ const n = Number(prompt('Yen?'));
+ const amount = (n / 138.91).toFixed(2); //小数点を切り上げる
+ console.log(`USD: ${Number(amount).toLocaleString()}`); //カンマを入れる
 }
 
