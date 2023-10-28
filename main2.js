b65b9b78eb95e477ba33c59485d47dd53f16f273
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-10-28 22:45:19 +0900

    [Finish]文字列操作編#03

--- main2.js
+++ main2.js
@@ -1,10 +1,21 @@
 'use strict';
 
 {
-  const string = prompt('Name?');
-  if (string.toLowerCase().trim() === 'taro') {
-    console.log('Correct!');
-  } else {
-    console.log('Wrong!');
-  }
+  const emails = [
+    'taro@example.com',
+    'kintaro@example.com',
+    'kojiro@example.com',
+  ];
+
+  emails.forEach((email) => {
+    // if (email.includes('taro') === true) {
+    //   console.log(email);
+    // }
+    // if (email.indexOf('taro') === 0) {
+    //   console.log(email);
+    // }
+    if (email.startsWith('taro') === true) {
+      console.log(email);
+    }
+  });
 }
